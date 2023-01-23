109b03f75ffd01b1ebc1a4388034ca390d631b27
Gifaldy Azka <falcxxdev@pm.me>
2023-01-23 16:36:55 +0700

    chore: repo cleanup (reset)

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import './assets/fonts.css';
-import './assets/tailwind.css';
-import './assets/nprogress.css';
-import App from './App.vue';
-import router from './router/router';
-
-createApp(App).use(router).mount('#app');
