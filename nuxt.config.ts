312ccb1e969a0e9c347247adbbda2679e3c326f0
Gifaldy Azka <falcxxdev@pm.me>
2023-01-23 17:38:34 +0700

    chore: add @nuxtjs/tailwindcss to modules[] entry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss'],
     builder: 'vite',
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+    },
 });
