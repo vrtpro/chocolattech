2d3804427b0a251f9dee25b91b29030ea25e3e12
Gifaldy Azka <falcxxdev@pm.me>
2023-01-24 19:30:55 +0700

    chore: add nuxt-icon to modules[] entry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
     builder: 'vite',
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
