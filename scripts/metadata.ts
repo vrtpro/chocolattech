960402dc9057ae34463fa2ffb6992f2738bd9bf3
Gifaldy Azka <falcxxdev@pm.me>
2023-01-25 20:44:47 +0700

    feat: add web metadata file

---
+++ scripts/metadata.ts
@@ -0,0 +1,17 @@
+export const _initHead = () => {
+    useHead({
+        titleTemplate: titleChunk => {
+            return titleChunk ? `${titleChunk} | Choco Berry website` : `Choco Berry website`;
+        },
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { 'http-equiv': 'x-ua-compatible', content: 'IE=edge' },
+            { name: 'charset', content: 'utf-8' },
+            { name: 'description', content: 'Choco Berry website with Nuxt 3' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+            { name: 'author', contnet: 'Choco Berry' },
+        ],
+    });
+};
