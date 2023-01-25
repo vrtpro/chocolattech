5959205844b875f3fe66714cd57fcf553e278f79
Gifaldy Azka <falcxxdev@pm.me>
2023-01-25 20:46:14 +0700

    fix: accessibility issues

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -5,26 +5,32 @@
         <header class="pt-4 px-4 lg:px-12 font-lexend">
             <nav class="mx-auto p-4 py-3 h-14 bg-[#ffb5cd] text-white flex justify-between shadow-lg rounded-2xl z-10">
                 <div class="w-fit">
-                    <NuxtLink to="/" class="navbar__link">
+                    <NuxtLink to="/" class="navbar__link" aria-label="Choco Logo">
                         <img
                             src="https://media.discordapp.net/attachments/953176156000104539/977521586393473024/logo.png"
                             alt="Logo"
                             class="w-24"
+                            width="0"
+                            height="0"
                         />
                     </NuxtLink>
                 </div>
                 <div class="bg-white h-8 text-chocolate-700 rounded-lg px-4 transition-all">
                     <ul class="flex gap-4 pt-[0.15rem]">
                         <li class="navbar__list">
-                            <NuxtLink to="/" class="navbar__link"><Icon name="material-symbols:home" /></NuxtLink>
+                            <NuxtLink to="/" class="navbar__link" aria-label="Home page"
+                                ><Icon name="material-symbols:home"
+                            /></NuxtLink>
                         </li>
                         <li class="navbar__list">
-                            <NuxtLink to="/about" class="navbar__link"
+                            <NuxtLink to="/about" class="navbar__link" aria-label="About page"
                                 ><Icon name="material-symbols:person"
                             /></NuxtLink>
                         </li>
                         <li class="navbar__list">
-                            <NuxtLink to="/models" class="navbar__link"><Icon name="mdi:tshirt-crew" /></NuxtLink>
+                            <NuxtLink to="/models" class="navbar__link" aria-label="Models page"
+                                ><Icon name="mdi:tshirt-crew"
+                            /></NuxtLink>
                         </li>
                     </ul>
                 </div>
