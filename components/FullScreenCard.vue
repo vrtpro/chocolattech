b08533d622bbb3fc38f9707776a50eb37b4473b6
Gifaldy Azka <falcxxdev@pm.me>
2023-01-27 18:35:01 +0700

    fix: darker background & more shadow

--- components/FullScreenCard.vue
+++ components/FullScreenCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="h-[95vh] bg-[#ffffff] bg-opacity-60 backdrop-blur-sm p-4 rounded-lg shadow-md">
+    <div class="h-[95vh] bg-[#efefef] bg-opacity-60 backdrop-blur-sm p-4 rounded-xl shadow-lg">
         <slot />
     </div>
 </template>
