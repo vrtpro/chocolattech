d05e91f960653d739b9af8cc0a3c5c9722d75208
Gifaldy Azka <falcxxdev@pm.me>
2023-01-27 18:36:40 +0700

    chore: complete home page (atleast for now)
    Changes may happen in future :sleeping: :sleeping: :sleeping:

--- pages/index.vue
+++ pages/index.vue
@@ -7,34 +7,31 @@ useHead({
 <template>
     <FullScreenCard class="lg:flex">
         <div class="hidden p-2 lg:block lg:py-16 lg:flex-1">
-            <h1 class="text-chocolate-800 text-center font-bold text-2xl uppercase">Image here</h1>
-            <p class="text-base text-chocolate-700 text-left">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ipsam quidem quis quam maiores dicta
-                suscipit repellendus id consequatur dolorem doloremque blanditiis voluptates obcaecati excepturi nihil.
-                Atque praesentium accusamus quasi quidem hic, illo nulla ut enim aliquid minima suscipit temporibus
-                pariatur necessitatibus laudantium voluptatibus, tempora, at fugiat omnis. Odio, modi!
-            </p>
+            <!-- TESTING PURPOSES ONLY. DOWNLOAD ASSETS & CHANGE TO LOCAL PATH! -->
+            <img
+                src="https://cdn.discordapp.com/attachments/953176156000104539/954697424960638976/Choco_with_Mahjong.png"
+                alt="Model"
+                class="h-[30rem] m-auto"
+            />
         </div>
         <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
-            <div>
-                <h1 class="text-2xl text-chocolate-800 text-center font-bold">Choco Berry</h1>
-                <p class="text-base text-chocolate-700 text-left">
-                    Aspiring virtual youtubers from another world. Lorem ipsum dolor, sit amet consectetur adipisicing
-                    elit. Laborum illum hic dolores, eum suscipit cumque deserunt earum fugiat, quasi cupiditate eius.
-                    Consequatur magni, non accusantium autem asperiores, ad deleniti placeat, dignissimos hic ullam eum
-                    tempora id delectus sit error debitis fuga esse. Quia repellendus necessitatibus labore excepturi
-                    aperiam sit possimus.
+            <div class="my-4 lg:mt-28">
+                <h1 class="text-2xl text-chocolate-800 text-center font-bold">Choco Berry チョコラテ</h1>
+                <h2 class="text-md text-slate-400 text-center font-bold italic">/ chocoberri /</h2>
+                <p class="text-base text-chocolate-700 my-3 text-center">
+                    A princess from Sweets Kingdom. She has power to become a cat. The princess accidentally enter a
+                    portal to the virtual world, so her power are lost and become a fully human! How she will back to
+                    her world?
                 </p>
             </div>
         </div>
         <div class="lg:hidden p-2 block lg:py-16 lg:flex-1">
-            <h1 class="text-chocolate-800 text-center font-bold text-2xl uppercase">Image here</h1>
-            <p class="text-base text-chocolate-700 text-left">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ipsam quidem quis quam maiores dicta
-                suscipit repellendus id consequatur dolorem doloremque blanditiis voluptates obcaecati excepturi nihil.
-                Atque praesentium accusamus quasi quidem hic, illo nulla ut enim aliquid minima suscipit temporibus
-                pariatur necessitatibus laudantium voluptatibus, tempora, at fugiat omnis. Odio, modi!
-            </p>
+            <!-- TESTING PURPOSES ONLY. DOWNLOAD ASSETS & CHANGE TO LOCAL PATH! -->
+            <img
+                src="https://cdn.discordapp.com/attachments/953176156000104539/954697424960638976/Choco_with_Mahjong.png"
+                alt="Model"
+                class="h-[30rem] m-auto"
+            />
         </div>
     </FullScreenCard>
 </template>
