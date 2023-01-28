4f8272f9366d3a6e21dc686089cd3c7c7d8f0ac3
Gifaldy Azka <falcxxdev@pm.me>
2023-01-29 00:53:51 +0700

    chore: revert back to choco latte

--- scripts/metadata.ts
+++ scripts/metadata.ts
@@ -1,7 +1,7 @@
 export const _initHead = () => {
     useHead({
         titleTemplate: titleChunk => {
-            return titleChunk ? `${titleChunk} | Choco Berry website` : `Choco Berry website`;
+            return titleChunk ? `${titleChunk} | Choco Latte website` : `Choco Latte website`;
         },
         htmlAttrs: {
             lang: 'en',
@@ -9,9 +9,9 @@ export const _initHead = () => {
         meta: [
             { 'http-equiv': 'x-ua-compatible', content: 'IE=edge' },
             { name: 'charset', content: 'utf-8' },
-            { name: 'description', content: 'Choco Berry website with Nuxt 3' },
+            { name: 'description', content: 'Choco Latte website with Nuxt 3' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-            { name: 'author', content: 'Choco Berry' },
+            { name: 'author', content: 'Choco Latte' },
         ],
     });
 };
