d97d0ce650e6ee8f8e6d8c8c0519ef440031038a
Gifaldy Azka <falcxxdev@pm.me>
2023-01-28 22:57:55 +0700

    feat: complete some pages (atleast for now)
    Complete models, commision, donation & merch page

---
+++ pages/merch.vue
@@ -0,0 +1,40 @@
+<template>
+    <FullScreenCard class="h-auto">
+        <FlexCard>
+            <div class="p-2 lg:py-16 lg:flex-1">
+                <!-- TESTING PURPOSES ONLY. DOWNLOAD ASSETS & CHANGE TO LOCAL PATH! -->
+                <img
+                    src="https://chocolatte.falcxxdev.ml/vite_assets/img/merch_1.jpg"
+                    alt="Merch 1"
+                    class="h-[22rem] m-auto rounded-xl shadow-md"
+                />
+            </div>
+            <div class="px-5 lg:py-auto lg:flex lg:w-1/2">
+                <div class="my-4 px-3 lg:mt-36 mx-auto">
+                    <h1 class="text-2xl text-chocolate-800 text-center font-bold">
+                        Phone case & Acrylic keychain Choco Latte
+                    </h1>
+                    <p class="text-base text-chocolate-700 my-3 text-center">Pre order @ Tokopedia</p>
+                    <a
+                        href="https://tokopedia.com/erfolganime"
+                        class="button w-full my-1 mb-4"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Icon
+                            name="material-symbols:shopping-cart"
+                            class="block flex-grow-0 shrink-0"
+                            width="32"
+                            height="32"
+                        />
+                        <span class="flex-grow flex-shrink text-left w-full">Buy Here!</span>
+                    </a>
+                </div>
+            </div>
+        </FlexCard>
+    </FullScreenCard>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
---
+++ pages/models.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts"></script>
+
+<template>
+    <FullScreenCard class="h-auto">
+        <FlexCard>
+            <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
+                <div class="my-4 lg:mt-28">
+                    <h1 class="text-2xl text-chocolate-800 text-center font-bold">Traveler - Original Model</h1>
+                    <p class="text-base text-chocolate-700 my-3 text-center">
+                        Choco loves making friends with everyone. Sometimes she will run away from castle to meet her
+                        friends. To make her more comfortable, she will use this costume.
+                    </p>
+                </div>
+            </div>
+            <div class="p-2 lg:py-16 lg:flex-1">
+                <!-- TESTING PURPOSES ONLY. DOWNLOAD ASSETS & CHANGE TO LOCAL PATH! -->
+                <img
+                    src="https://cdn.discordapp.com/attachments/953176156000104539/953177023495405599/Choco_Latte_Assets.png"
+                    alt="Traveler model"
+                    class="h-[25rem] m-auto"
+                />
+            </div>
+        </FlexCard>
+        <FlexCard>
+            <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
+                <div class="my-4 lg:mt-36 mx-auto">
+                    <h1 class="text-2xl text-chocolate-800 text-center font-bold">Princess model - 2022 Project</h1>
+                    <p class="text-base text-chocolate-700 my-3 text-center">To be Announced @ 2022</p>
+                </div>
+            </div>
+            <div class="p-2 lg:py-16 lg:flex-1">
+                <!-- TESTING PURPOSES ONLY. DOWNLOAD ASSETS & CHANGE TO LOCAL PATH! -->
+                <img
+                    src="https://chocolatte.falcxxdev.ml/vite_assets/img/princess_model.png"
+                    alt="Princess model"
+                    class="h-[25rem] m-auto"
+                />
+            </div>
+        </FlexCard>
+        <FlexCard>
+            <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
+                <div class="my-4 lg:mt-36 mx-auto">
+                    <h1 class="text-2xl text-chocolate-800 text-center font-bold">Loli model</h1>
+                    <p class="text-base text-chocolate-700 my-3 text-center">To be Announced</p>
+                </div>
+            </div>
+            <div class="p-2 lg:py-16 lg:flex-1">
+                <!-- TESTING PURPOSES ONLY. DOWNLOAD ASSETS & CHANGE TO LOCAL PATH! -->
+                <img
+                    src="https://chocolatte.falcxxdev.ml/vite_assets/img/loli_model.png"
+                    alt="Loli model"
+                    class="h-[25rem] m-auto"
+                />
+            </div>
+        </FlexCard>
+    </FullScreenCard>
+</template>
+
+<style></style>
