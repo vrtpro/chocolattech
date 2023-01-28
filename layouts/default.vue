3969276cbd168a5f256276a85648c50c3a283d92
Gifaldy Azka <falcxxdev@pm.me>
2023-01-29 02:20:47 +0700

    chore: change progress color to chocolate-800

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@ _initHead();
 
 <template>
     <Navigation />
-    <NuxtLoadingIndicator color="var(--gradient)" />
+    <NuxtLoadingIndicator color="#6E5050" />
     <div class="container mx-auto p-4 font-lexend">
         <slot />
     </div>
@@ -12,9 +12,6 @@ _initHead();
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap');
-:root {
-    --gradient: linear-gradient(to right, #0ea5e9, #8b5cf6, #f67185);
-}
 .page-enter-active,
 .page-leave-active {
     transition: all 0.4s;
