bb85ae3ef44ffc70821815dc007997856288c885
Gifaldy Azka <falcxxdev@pm.me>
2023-02-14 20:52:12 +0700

    fix: wider space & margin-top adjustments

--- pages/models.vue
+++ pages/models.vue
@@ -22,12 +22,13 @@ useHead({
         </FlexCard>
         <FlexCard>
             <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
-                <div class="my-4 lg:mt-36 mx-auto">
+                <div class="my-4 lg:mt-28 mx-auto">
                     <h1 class="text-2xl text-chocolate-800 text-center font-bold">Princess model - 2022 Project</h1>
                     <p class="text-base text-chocolate-700 my-3 text-center">
                         Newly-introduced outfit.
                         <br />
                         Officially introduced on February 14, 2023 at 7:00 GMT+7
+                        <br />
                         <br />
                         P.S: This model is too gorgeous. Be careful handling your diabetes level!
                     </p>
@@ -39,13 +40,15 @@ useHead({
         </FlexCard>
         <FlexCard>
             <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
-                <div class="my-4 lg:mt-36 mx-auto">
+                <div class="my-4 lg:mt-28 mx-auto">
                     <h1 class="text-2xl text-chocolate-800 text-center font-bold">Loli model</h1>
                     <p class="text-base text-chocolate-700 my-3 text-center">
                         Newly-introduced outfit too.
                         <br />
-                        Officially introduced on February 14, 2023 at 8:10 PM GMT+7 after releasing her cover song "song
-                        for you".
+                        Officially introduced on February 14, 2023 at 8:10 PM GMT+7
+                        <br />
+                        after releasing her cover song "song for you".
+                        <br />
                         <br />
                         P.S: Model picture coming soon. This model is adopt-able!
                     </p>
