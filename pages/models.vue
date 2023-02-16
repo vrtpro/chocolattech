c47ed8186f48df9b9cb568a4c99edd13c7ec6177
Gifaldy Azka <falcxxdev@pm.me>
2023-02-16 17:48:02 +0700

    chore: update loli model picture

--- pages/models.vue
+++ pages/models.vue
@@ -40,7 +40,7 @@ useHead({
         </FlexCard>
         <FlexCard>
             <div class="p-2 lg:py-16 lg:flex lg:w-1/2">
-                <div class="my-4 lg:mt-28 mx-auto">
+                <div class="my-4 lg:mt-32 mx-auto">
                     <h1 class="text-2xl text-chocolate-800 text-center font-bold">Loli model</h1>
                     <p class="text-base text-chocolate-700 my-3 text-center">
                         Newly-introduced outfit too.
@@ -48,9 +48,6 @@ useHead({
                         Officially introduced on February 14, 2023 at 8:10 PM GMT+7
                         <br />
                         after releasing her cover song "song for you".
-                        <br />
-                        <br />
-                        P.S: Model picture coming soon. This model is adopt-able!
                     </p>
                 </div>
             </div>
