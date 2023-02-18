1d6165b7b512c37a2a52aa4eac2ee0be328206fe
Gifaldy Azka <falcxxdev@pm.me>
2023-02-18 23:24:41 +0700

    feat: add choco's signature to bottom right of the card

--- components/FullScreenCard.vue
+++ components/FullScreenCard.vue
@@ -3,6 +3,11 @@
 <template>
     <div class="h-[95vh] bg-[#efefef] bg-opacity-60 backdrop-blur-sm p-4 rounded-xl shadow-lg">
         <slot />
+        <img
+            src="/img/signature.webp"
+            alt="Signature"
+            class="flex absolute h-16 bottom-[-20px] right-[-30px] -rotate-12 pointer-events-none sign"
+        />
     </div>
 </template>
 
