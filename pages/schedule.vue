157ca08c422870aa4e75bcc8dc7b09c5e599868b
Gifaldy Azka <falcxxdev@pm.me>
2023-02-18 08:33:42 +0700

    feat: add schedule route
    Weekly schedule route

---
+++ pages/schedule.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+    <FullScreenCard class="h-auto">
+        <div class="p-2">
+            <h1 class="text-3xl text-chocolate-700 font-bold text-center">Live Schedule</h1>
+            <FullScreenCard class="lg:w-[50rem] h-auto m-auto">
+                <div>
+                    <img
+                        src="https://cdn.discordapp.com/attachments/949169677920268339/1075083750217613355/Skejul.png"
+                        alt="Weekly schedule"
+                        class="rounded-xl shadow-md hover:ring-2 ring-chocolate-700 transition-all"
+                    />
+                    <p class="text-chocolate-700 text-sm mt-3">
+                        Schedule for February 12, 2023 - February 18, 2023
+                        <br />
+                        Last updated on <code class="text-[#ff93b5] font-bold">Sat Feb 18 2023 08:17:20 GMT+7</code>
+                    </p>
+                    <a
+                        href="https://www.youtube.com/@ChocoLatteCh"
+                        target="_blank"
+                        rel="noreferrer nofollow me"
+                        class="button w-full lg:w-96 my-3"
+                    >
+                        <Icon name="mdi:youtube" class="block flex-grow-0 shrink-0" width="32" height="32" />
+                        <span class="flex-grow flex-shrink text-left w-full">Watch stream on YouTube</span>
+                    </a>
+                </div>
+            </FullScreenCard>
+        </div>
+    </FullScreenCard>
+</template>
+
+<style></style>
