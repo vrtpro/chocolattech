79714adcf12382857ed421eca905e75753def7b1
Gifaldy Azka <falcxxdev@pm.me>
2023-03-02 21:36:49 +0700

    fix: remove vueuse/motion from modules entry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@vueuse/motion/nuxt', 'nuxt-icon'],
+    modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
     builder: 'vite',
     app: {
         pageTransition: {
