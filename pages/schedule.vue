a2f9cfc2c6cdca992405ef527aba7027b20504f2
Gifaldy Azka <falcxxdev@pm.me>
2023-06-12 21:37:41 +0700

    chore(schedule): set to nostream - bye stream!

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -11,14 +11,14 @@ useHead({
             <FullScreenCard class="lg:w-[50rem] h-auto m-auto schedule">
                 <div>
                     <img
-                        src="https://cdn.discordapp.com/attachments/1082594335775793182/1109778278165794876/minified_schedule_5.webp"
+                        src="https://cdn.discordapp.com/attachments/898543182189559880/1080883498266661015/nostream.webp"
                         alt="Weekly schedule"
                         class="rounded-xl shadow-md hover:ring-2 ring-chocolate-700 transition-all"
                     />
                     <p class="text-chocolate-700 text-sm mt-3">
-                        Schedule for May 21 to May 27
+                        Schedule for unknown (little chance to stream)
                         <br />
-                        Last updated on <code class="text-[#ff93b5] font-bold">Sun May 21 2023 16:49:00 GMT+7</code>
+                        Last updated on <code class="text-[#ff93b5] font-bold">Mon Jun 12 2023 19:21:00 GMT+7</code>
                     </p>
                     <a
                         href="https://www.youtube.com/@ChocoLatteCh"
@@ -27,7 +27,7 @@ useHead({
                         class="button w-full lg:w-96 my-3"
                     >
                         <Icon name="mdi:youtube" class="block flex-grow-0 shrink-0" width="32" height="32" />
-                        <span class="flex-grow flex-shrink text-left w-full">Watch stream on YouTube</span>
+                        <span class="flex-grow flex-shrink text-left w-full">Watch past stream on YouTube</span>
                     </a>
                 </div>
             </FullScreenCard>
