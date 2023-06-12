f6d55c9ccebd612c2976065275cad9fa52e112a7
Gifaldy Azka <falcxxdev@pm.me>
2023-06-12 21:38:42 +0700

    feat(component): add modal (slot) component

---
+++ components/Modal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const state = ref(true);
+const close = () => {
+    state.value = !state.value;
+    document.body.classList.remove('overflow-hidden');
+};
+
+onMounted(() => {
+    if (state.value) {
+        document.body.classList.add('overflow-hidden');
+    }
+});
+</script>
+
+<template>
+    <div v-if="state" class="fixed z-30 flex flex-col items-center content-center bg-black bg-opacity-40 w-full h-full">
+        <div class="mt-2 cursor-pointer text-white hover:text-red-400" @click="close">
+            <Icon name="mdi:close-circle-outline" size="2.5em" />
+        </div>
+        <div class="bg-white mt-10 p-7 rounded-lg">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
