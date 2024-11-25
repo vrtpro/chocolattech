a34febf9cb25168ee130c1a508a11719303ca12d
Azka <falcxxdev@pm.me>
2024-11-25 10:19:49 +0000

    chore(nuxt): add compabilityDate & devtools object

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    devtools: { enabled: false },
     modules: ['@nuxtjs/tailwindcss', 'nuxt-jsonld', 'nuxt-icon'],
+    compatibilityDate: '2024-09-06',
     builder: 'vite',
     app: {
         pageTransition: {
